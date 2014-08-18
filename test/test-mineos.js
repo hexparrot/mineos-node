2dac8a57aaa2648ad5ede023a63d2792c4ae2419
William Dizon <wdchromium@gmail.com>
2014-08-18 13:50:43 -0700

    added teardown cleanup and create server func

--- test/test-mineos.js
+++ test/test-mineos.js
@@ -5,6 +5,16 @@ var mineos = require('../mineos/mineos');
 
 var test = exports;
 
+test.tearDown = function(callback) {
+	var server_list = mineos.server_list('/var/games/minecraft');
+	for (var i in server_list) {
+		fs.unlink( path.join('/var/games/minecraft/servers', server_list[i], 'server.properties') );
+		fs.unlink( path.join('/var/games/minecraft/servers', server_list[i], 'server.config') );
+		fs.rmdirSync(path.join('/var/games/minecraft/servers', server_list[i]));
+	}
+	callback();
+}
+
 test.server_list = function (test) {
     test.ok(mineos.server_list('/var/games/minecraft') instanceof Array, "server returns an array");
     test.done();
@@ -21,7 +31,22 @@ test.is_server = function(test) {
 	touch.sync(sp_path);
 	test.ok(mineos.is_server(server_path), 'newly created path + sp should succeed');
 
-	fs.unlink(sp_path);
-	fs.rmdirSync(server_path);
+	test.done();
+}
+
+test.create_server = function(test) {
+	var server_name = 'ccc';
+	var base_dir = '/var/games/minecraft';
+	var server_path = path.join(base_dir, 'servers', server_name);
+
+	test.equal(mineos.server_list(base_dir).length, 0);
+
+	test.ok(!mineos.is_server(server_path));
+	fs.mkdirSync(server_path);
+	touch.sync(path.join(server_path, 'server.properties'));
+	touch.sync(path.join(server_path, 'server.config'));
+
+	test.equal(mineos.server_list(base_dir)[0], server_name);
+	test.equal(mineos.server_list(base_dir).length, 1);
 	test.done();
 }
