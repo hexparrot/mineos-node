da31e532932d28cca2c00cfd49a6b63f8ec947a5
William Dizon <wdchromium@gmail.com>
2014-08-18 12:09:03 -0700

    changed node export pattern

--- mineos/mineos.js
+++ mineos/mineos.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 var path = require('path');
+var mineos = exports;
 
-exports.server_list = function(base_dir) {
+mineos.server_list = function(base_dir) {
   return fs.readdirSync(path.join(base_dir, 'servers'));
 }
 
-exports.is_server = function(server_path) {
+mineos.is_server = function(server_path) {
   return fs.existsSync(path.join(server_path, 'server.properties'));
 }
--- test/test-mineos.js
+++ test/test-mineos.js
@@ -3,12 +3,14 @@ var path = require('path');
 var touch = require("touch");
 var mineos = require('../mineos/mineos');
 
-exports['server_list'] = function (test) {
+var test = exports;
+
+test.server_list = function (test) {
     test.ok(mineos.server_list('/var/games/minecraft') instanceof Array, "server returns an array");
     test.done();
 };
 
-exports['is_server'] = function(test) {
+test.is_server = function(test) {
 	var server_name = 'testing';
 	var base_dir = '/var/games/minecraft';
 	var server_path = path.join(base_dir, server_name);
