5c22f7854ff4d8582a5e8b0cfadbb0a559e65812
William Dizon <wdchromium@gmail.com>
2014-08-19 07:48:41 -0700

    refactored out base_dir
    replaced fs with fs.extra

--- test/test-mineos.js
+++ test/test-mineos.js
@@ -1,29 +1,26 @@
-var fs = require('fs');
+var fs = require('fs.extra');
 var path = require('path');
 var touch = require("touch");
 var mineos = require('../mineos/mineos');
-
 var test = exports;
+var BASE_DIR = '/var/games/minecraft';
 
 test.tearDown = function(callback) {
-  var server_list = mineos.server_list('/var/games/minecraft');
+  var server_list = mineos.server_list(BASE_DIR);
   for (var i in server_list) {
-    fs.unlink( path.join('/var/games/minecraft/servers', server_list[i], 'server.properties') );
-    fs.unlink( path.join('/var/games/minecraft/servers', server_list[i], 'server.config') );
-    fs.rmdirSync(path.join('/var/games/minecraft/servers', server_list[i]));
+    fs.rmrfSync(path.join(BASE_DIR, 'servers', server_list[i]));
   }
   callback();
 }
 
 test.server_list = function (test) {
-    test.ok(mineos.server_list('/var/games/minecraft') instanceof Array, "server returns an array");
+    test.ok(mineos.server_list(BASE_DIR) instanceof Array, "server returns an array");
     test.done();
 };
 
 test.is_server = function(test) {
   var server_name = 'testing';
-  var base_dir = '/var/games/minecraft';
-  var server_path = path.join(base_dir, server_name);
+  var server_path = path.join(BASE_DIR, 'servers', server_name);
   var sp_path = path.join(server_path, 'server.properties');
 
   test.ok(!mineos.is_server(server_path), 'non-existent path should fail');
@@ -36,17 +33,17 @@ test.is_server = function(test) {
 
 test.create_server = function(test) {
   var server_name = 'ccc';
-  var base_dir = '/var/games/minecraft';
-  var server_path = path.join(base_dir, 'servers', server_name);
+  var server_path = path.join(BASE_DIR, 'servers', server_name);
 
-  test.equal(mineos.server_list(base_dir).length, 0);
+  test.equal(mineos.server_list(BASE_DIR).length, 0);
 
   test.ok(!mineos.is_server(server_path));
   fs.mkdirSync(server_path);
   touch.sync(path.join(server_path, 'server.properties'));
   touch.sync(path.join(server_path, 'server.config'));
 
-  test.equal(mineos.server_list(base_dir)[0], server_name);
-  test.equal(mineos.server_list(base_dir).length, 1);
+  test.equal(mineos.server_list(BASE_DIR)[0], server_name);
+  test.equal(mineos.server_list(BASE_DIR).length, 1);
+  
   test.done();
 }
