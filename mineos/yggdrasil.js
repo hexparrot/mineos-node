27ecb0f840868f73a748498d1d7c8c31aaaafce9
William Dizon <wdchromium@gmail.com>
2014-08-27 10:33:20 -0700

    refactored yggdrasil.authenticate

--- mineos/yggdrasil.js
+++ mineos/yggdrasil.js
@@ -1,10 +1,10 @@
-var path = require('path');
 var request = require('request');
-var async = require('async');
+var url = require('url');
 var yggdrasil = exports;
 
+yggdrasil.SERVER_HOST = 'https://authserver.mojang.com';
+
 yggdrasil.authenticate = function(username, password, callback) {
-  var endpoint = 'authenticate';
   var auth = {
     "agent": {
       "name": "Minecraft",
@@ -15,7 +15,7 @@ yggdrasil.authenticate = function(username, password, callback) {
   }
 
   request.post(
-    'https://authserver.mojang.com/authenticate', { json: auth }, function (error, response, body) {
+    url.resolve(yggdrasil.SERVER_HOST, 'authenticate'), { json: auth }, function (error, response, body) {
       if (!error && response.statusCode == 200) 
         callback(body);
     }
