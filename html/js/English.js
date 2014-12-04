e49e855f4d3f20a493d27822902497e8b12485fc
Peter Salzmann <petersalzmann@gmail.com>
2014-12-04 19:41:52 +1030

    updated login to use external file for localisation English.js contains array of sentences to parse in.

---
+++ html/js/English.js
@@ -0,0 +1,11 @@
+var English = [
+{
+	name: 'English',
+	header: 'Web User Interface Login',
+	username: 'Username',
+	password: 'Password',
+	submit: 'Login',
+	mask: 'Hide password',
+	info: 'The web user interface authenticates on your SSH username/password.'
+}
+]
