9c7edd3bfe05d19c523c82f2fbf7f7b961faca4d
William Dizon <wdchromium@gmail.com>
2015-03-29 21:06:05 -0700

    angular-translate implemented
    translations are object coded in-line; will be refactored out

--- html/js/scriptin.js
+++ html/js/scriptin.js
@@ -1,5 +1,21 @@
-var app = angular.module("mineos", ['angularMoment']);
+var app = angular.module("mineos", ['angularMoment', 'pascalprecht.translate']);
 var connect_string = ':3000/';
+
+app.config(function ($translateProvider) {
+  $translateProvider.translations('en', {
+    TITLE: 'Hello',
+    FOO: 'This is a paragraph.',
+    BUTTON_LANG_EN: 'english',
+    BUTTON_LANG_DE: 'german'
+  });
+  $translateProvider.translations('de', {
+    TITLE: 'Hallo',
+    FOO: 'Dies ist ein Paragraph.',
+    BUTTON_LANG_EN: 'englisch',
+    BUTTON_LANG_DE: 'deutsch'
+  });
+  $translateProvider.preferredLanguage('en');
+});
 
 /* directives */
 
