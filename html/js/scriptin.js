882ade897edd1c1f6d19bc6bafc260af270a535f
William Dizon <wdchromium@gmail.com>
2015-04-20 16:40:58 -0700

    various ui tweaks

--- html/js/scriptin.js
+++ html/js/scriptin.js
@@ -387,7 +387,12 @@ app.factory("Servers", ['socket', '$filter', function(socket, $filter) {
         me.latest_notice[data.command] = data;
 
         switch(data.command) {
+          case 'modify_sp':
+            me.channel.emit(server_name, 'property', {property: 'server.properties'});
+            data['suppress_popup'] = true;
+            break;
           case 'modify_sc':
+            me.channel.emit(server_name, 'property', {property: 'server.config'});
             data['suppress_popup'] = true;
             break;
           case 'restore':
