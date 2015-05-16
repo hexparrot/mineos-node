68a62421fbf8c0b739c0dd45e31c716d64e36024
William Dizon <wdchromium@gmail.com>
2015-05-16 13:52:50 -0700

    added daemon script

---
+++ service.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var daemon = require("daemonize2").setup({
+    main: "webui.js",
+    name: "mineos",
+    pidfile: "mineos.pid"
+});
+
+if (process.getuid() != 0) {
+    console.log("Expected to run as root");
+    process.exit(1);
+}
+
+switch (process.argv[2]) {
+    case "start":
+        daemon.start();
+        break;
+    case "stop":
+        daemon.stop();
+        break;
+    case "restart":
+        daemon.stop(function(err) {
+            daemon.start();
+        });
+        break;
+    case "status":
+        var pid = daemon.status();
+        if (pid)
+            console.log("MineOS running. PID: " + pid);
+        else
+            console.log("MineOS is not running.");
+        break;
+    default:
+        console.log("Usage: [start|stop|restart|status]");
+}
