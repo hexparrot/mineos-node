66cec40a12662cbb734dda1aaabc21735b3fef03
William Dizon <wdchromium@gmail.com>
2015-06-25 15:03:02 -0700

    cherrypick freebsd update

--- auth.js
+++ auth.js
@@ -2,27 +2,51 @@ var async = require('async');
 var auth = exports;
 
 auth.authenticate_shadow = function(user, plaintext, callback) {
-  var passwd = require('etc-passwd');
   var hash = require('sha512crypt-node');
+  var fs = require('fs-extra');
 
-  async.waterfall([
-    async.apply(passwd.getShadow, {username: user}),
-    function(shadow_info, cb) {
-      try {
-        var password_parts = shadow_info['password'].split(/\$/);
-        var salt = password_parts[2];
-        var new_hash = hash.sha512crypt(plaintext, salt);
+  function etc_shadow(inner_callback) {
+    var passwd = require('etc-passwd');
+    async.waterfall([
+      async.apply(fs.stat, '/etc/shadow'),
+      async.apply(passwd.getShadow, {username: user}),
+      function(shadow_info, cb) {
+        try {
+          var password_parts = shadow_info['password'].split(/\$/);
+          var salt = password_parts[2];
+          var new_hash = hash.sha512crypt(plaintext, salt);
 
-        cb(null, new_hash == shadow_info['password']);
-      } catch (e) {
-        cb(true, null);
+          cb(null, new_hash == shadow_info['password']);
+        } catch (e) {
+          cb(true, null);
+        }
       }
+    ], inner_callback)
+  }
+
+  function posix(inner_callback) {
+    var crypt = require('crypt3');
+    var posix = require('posix');
+
+    try {
+      var user_data = posix.getpwnam(user);
+      if (crypt(plaintext, user_data.passwd) == user_data.passwd)
+        inner_callback(user);
+      else
+        inner_callback(false);
+    } catch (e) {
+      inner_callback(false);
     }
-  ], function(err, retval) {
-    if (retval)
-      callback(user);
-    else
-      callback(false);
+  }
+
+  etc_shadow(function(err, result_one) {
+    if (err.errno == -2) { //if /etc/shadow ENOENT
+      posix(function(result_two) {
+        callback(result_two)
+      })
+    } else { //if /etc/shadow was found
+      callback(result_one);
+    }
   })
 }
 
