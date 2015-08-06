c75ca30ba93b3561d169bc30f0b41b105036187c
William Dizon <wdchromium@gmail.com>
2015-08-06 08:54:53 -0700

    fully functionalized mineos_console.js

--- mineos_console.js
+++ mineos_console.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
 var getopt = require('node-getopt');
-var async = require('async');
-var introspect = require('introspect');
-var child_process = require('child_process');
 var mineos = require('./mineos');
 
 var opt = getopt.create([
@@ -17,6 +14,8 @@ var opt = getopt.create([
 .parseSystem(); // parse command line
 
 function return_git_commit_hash(callback) {
+  var child_process = require('child_process');
+
   var gitproc = child_process.spawn('git', 'log -n 1 --pretty=format:"%H"'.split(' '));
   var commit_value = '';
 
@@ -39,84 +38,104 @@ function return_git_commit_hash(callback) {
   });
 }
 
+function handle_server(args, callback) {
+  var introspect = require('introspect');
+
+  var base_dir = (args.options || {}).d || '/var/games/minecraft';
+  var command = args.argv.shift();
+  var fn = instance[command];
+  var arg_array = [];
+  var required_args = introspect(fn);
+
+  while (required_args.length) {
+    var ra = required_args.shift();
+
+    switch (ra) {
+      case 'callback':
+        arg_array.push(function(err, payload) {
+          var retval = [];
+
+          if (!err) {
+            retval.push('[{0}] Successfully executed "{1}"'.format(args.options.server_name, command));
+            if (payload)
+              retval.push(payload)
+          } else {
+            retval.push('[{0}] Error executing "{1}" because server condition not met: {2}'.format(
+              args.options.server_name, 
+              command,
+              err)
+            );
+          }
+
+          callback( (err ? 1 : 0), retval );
+        })
+        break;
+      case 'owner':
+        try {
+          var owner_pair = opt.argv.shift().split(':');
+          if (owner_pair.length != 2)
+            throw 'err';
+          arg_array.push({
+            uid: parseInt(owner_pair[0]),
+            gid: parseInt(owner_pair[1])
+          })
+        } catch (e) {
+          console.log('Provide owner attribute as uid:gid pair, e.g., 1000:1000');
+          process.exit(9);
+        } 
+        break;
+      default:
+        arg_array.push(opt.argv.shift())
+        break;
+    } //end switch
+  } //end while
+
+  fn.apply(instance, arg_array); //actually run the function with the args
+}
+
+function retrieve_property(args, callback) {
+  var property = args.argv.shift();
+  var fn = instance.property;
+  var arg_array = [property];
+  var retval = [];
+
+  arg_array.push(function(err, payload) {
+    if (!err && payload !== undefined) {
+      retval.push('[{0}] Queried property: "{1}"'.format(args.options.server_name, property));
+      retval.push(payload);
+    } else {
+      retval.push('[{0}] Error querying property "{1}"'.format(
+        args.options.server_name, 
+        property,
+        err));
+    }
+    callback( (err ? 1 : 0), retval);
+  })
+
+  fn.apply(instance, arg_array);
+}
+
+var base_dir = (opt.options || {}).d || '/var/games/minecraft';
+var instance = new mineos.mc(opt.options.server_name, base_dir);
+
 if ('version' in opt.options) {
   return_git_commit_hash(function(code, hash) {
     if (!code)
       console.log(hash);
     process.exit(code);
   })
-} else if ('server_name' in opt.options) {
-  var base_dir = opt.options.d || '/var/games/minecraft';
-  var instance = new mineos.mc(opt.options.server_name, base_dir);
-
-  if (opt.argv[0] in instance) {
-    var command = opt.argv.shift();
-    var fn = instance[command];
-    var arg_array = [];
-    var required_args = introspect(fn);
-
-    while (required_args.length) {
-      var argument = required_args.shift();
-
-      switch (argument) {
-        case 'callback':
-          arg_array.push(function(err, payload) {
-            if (!err) {
-              console.log('[{0}] Successfully executed "{1}"'.format(opt.options.server_name, command));
-              if (payload)
-                console.log(payload)
-              process.exit(0);
-            } else {
-              console.log('[{0}] Error executing "{1}" because server condition not met: {2}'.format(
-                opt.options.server_name, 
-                command,
-                err));
-              process.exit(1);
-            }
-          })
-          break;
-        case 'owner':
-          try {
-            var owner_pair = opt.argv.shift().split(':');
-            if (owner_pair.length != 2)
-              throw 'err';
-            arg_array.push({
-              uid: parseInt(owner_pair[0]),
-              gid: parseInt(owner_pair[1])
-            })
-          } catch (e) {
-            console.log('Provide owner attribute as uid:gid pair, e.g., 1000:1000');
-            process.exit(9);
-          } 
-          break;
-        default:
-          arg_array.push(opt.argv.shift())
-          break;
-      }
-    }
-
-    fn.apply(instance, arg_array);
-  } else {
-    var property = opt.argv.shift();
-    var fn = instance.property;
-    var arg_array = [property];
-
-    arg_array.push(function(err, payload) {
-      if (!err && payload !== undefined) {
-        console.log('[{0}] Queried property: "{1}"'.format(opt.options.server_name, property));
-        console.log(payload);
-        process.exit(0);
-      } else {
-        console.log('[{0}] Error querying property "{1}"'.format(
-          opt.options.server_name, 
-          property,
-          err));
-        process.exit(1);
-      }
-    })
-
-    fn.apply(instance, arg_array);
-  }
+} else if (opt.argv[0] in instance) { //first provided param matches a function name) {
+  handle_server(opt, function(code, retval) {
+    for (var idx in retval)
+      console.log(retval[idx])
+    process.exit(code);
+  })
+} else {
+  retrieve_property(opt, function(code, retval) {
+    for (var idx in retval)
+      console.log(retval[idx])
+    process.exit(code);
+  })
 }
 
 String.prototype.format = function() {
