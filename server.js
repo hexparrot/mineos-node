79d800bb185824947f4ee3c103ad245ab78a9b75
William Dizon <wdchromium@gmail.com>
2015-09-09 14:32:23 -0700

    added forge to webui

--- server.js
+++ server.js
@@ -1096,6 +1096,38 @@ function check_profiles(base_dir, callback) {
       }
       request({ url: FTB_VERSIONS_URL, json: false }, handle_reply);
     },
+    forge: function(callback) {
+      var FORGE_VERSIONS_URL = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions.json';
+      var path_prefix = path.join(base_dir, mineos.DIRS['profiles']);
+
+      function handle_reply(err, response, body) {
+        var p = [];
+
+        if (!err && (response || {}).statusCode === 200)
+          for (var index in body.promos) {
+            var item = new profile_template();
+            var ref_obj = body.promos[index];
+
+            item['id'] = index;
+            item['time'] = ref_obj['modified'];
+            item['releaseTime'] = ref_obj['modified'];
+            item['type'] = 'release';
+            item['group'] = 'forge';
+            item['webui_desc'] = 'Forge Jar (build {0})'.format(ref_obj['build']);
+            item['weight'] = 0;
+            item['filename'] = 'forge-{0}-{1}-universal.jar'.format(ref_obj['mcversion'], ref_obj['version']);
+            item['downloaded'] = fs.existsSync(path.join(base_dir, mineos.DIRS['profiles'], item.id, item.filename));
+            item['version'] = ref_obj['mcversion'];
+            item['release_version'] = ref_obj['version'];
+            item['url'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/{0}-{1}/{2}'.format(ref_obj['mcversion'], ref_obj['version'], item['filename']);
+
+            p.push(item);
+          }
+
+        callback(err, p);
+      }
+      request({ url: FORGE_VERSIONS_URL, json: true }, handle_reply);
+    },
     pocketmine: function(callback) {
       var URL_DEVELOPMENT = "http://www.pocketmine.net/api/?channel=development";
       var URL_STABLE = "http://www.pocketmine.net/api/?channel=stable";
@@ -1358,6 +1390,46 @@ function download_profiles(base_dir, args, progress_update_fn, callback) {
                 logging.error('[WEBUI] Remote server returned status {0} with headers:'.format(response.statusCode), response.headers);
                 args['success'] = false;
                 args['help_text'] = 'Remote server did not return {0} (status {1})'.format(args.filename, response.statusCode);
+                inner_callback(args);
+              }
+            })
+            .on('progress', function(state) {
+              args['progress'] = state;
+              progress_update_fn(args);
+            })
+            .pipe(fs.createWriteStream(dest_filepath))
+        }
+      });
+    },
+    forge: function(inner_callback) {
+      var dest_dir = path.join(base_dir, 'profiles', args.id);
+      var dest_filepath = path.join(dest_dir, args.filename);
+
+      var url = args.url;
+
+      fs.ensureDir(dest_dir, function(err) {
+        if (err) {
+          logging.error('[WEBUI] Error attempting download:', err);
+        } else {
+          progress(request(url), {
+            throttle: 1000,
+            delay: 100
+          })
+            .on('complete', function(response) {
+              if (response.statusCode == 200) {
+                logging.log('[WEBUI] Successfully downloaded {0} to {1}'.format(url, dest_filepath));
+                args['dest_dir'] = dest_dir;
+                args['success'] = true;
+                args['progress']['percent'] = 100;
+                args['help_text'] = 'Successfully downloaded {0} to {1}'.format(url, dest_filepath);
+                args['suppress_popup'] = false;
+                inner_callback(args);
+              } else {
+                logging.error('[WEBUI] Server was unable to download file:', url);
+                logging.error('[WEBUI] Remote server returned status {0} with headers:'.format(response.statusCode), response.headers);
+                args['success'] = false;
+                args['help_text'] = 'Remote server did not return {0} (status {1})'.format(args.filename, response.statusCode);
+                args['suppress_popup'] = false;
                 inner_callback(args);
               }
             })
