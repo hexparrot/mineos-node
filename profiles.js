7aa4ed6c0abcd4e09058f7e9128010391e30b60c
William Dizon <wdchromium@gmail.com>
2017-07-24 16:23:43 +0000

    added nukkit stable/snapshot to profiles

--- profiles.js
+++ profiles.js
@@ -618,4 +618,48 @@ exports.profile_manifests = {
       ], callback)
     }
   },
+  nukkit: {
+    name: 'Nukkit',
+    handler: function(profile_dir, callback) {
+      var p = [];
+
+      try {
+        var item = {};
+
+        item['id'] = 'nukkit-stable';
+        item['time'] = new Date().getTime();
+        item['releaseTime'] = new Date().getTime();
+        item['type'] = 'release';
+        item['group'] = 'nukkit';
+        item['webui_desc'] = 'Minecraft: PE server for Java (stable)';
+        item['weight'] = 0;
+        item['filename'] = 'nukkit-1.0-SNAPSHOT.jar';
+        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
+        item['version'] = 0;
+        item['release_version'] = '';
+        item['url'] = 'http://ci.mengcraft.com:8080/job/nukkit/lastStableBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar';
+
+        p.push(item);
+
+        var item = {};
+
+        item['id'] = 'nukkit-snapshot';
+        item['time'] = new Date().getTime();
+        item['releaseTime'] = new Date().getTime();
+        item['type'] = 'snapshot';
+        item['group'] = 'nukkit';
+        item['webui_desc'] = 'Minecraft: PE server for Java (last successful)';
+        item['weight'] = 0;
+        item['filename'] = 'nukkit-1.0-SNAPSHOT.jar';
+        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
+        item['version'] = 0;
+        item['release_version'] = '';
+        item['url'] = 'http://ci.mengcraft.com:8080/job/nukkit/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar';
+
+        p.push(item);
+      } catch (e) {}
+
+      callback(null, p);
+    } //end handler
+  },
 };
