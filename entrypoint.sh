a2944f738f941ad5cebb7adf6e30d8324656f343
Kyle Klaus <kklaus@indemnity83.com>
2020-03-27 12:20:15 -0700

    use GROUP_GID
    instead of USER_GID for clarity and variable consistency

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,29 +38,29 @@ else
   USER_UID=1000
 fi
 
-if [ "$USER_GID" ]; then
+if [ "$GROUP_GID" ]; then
   # gid specifically provided, will overwrite 1000 default
-  if [[ "$USER_GID" =~ [^0-9] ]]; then
-    echo >&2 'USER_GID must contain only numerics [0-9]'
+  if [[ "$GROUP_GID" =~ [^0-9] ]]; then
+    echo >&2 'GROUP_GID must contain only numerics [0-9]'
     exit 1
   fi
 else
-  USER_GID=1000
+  GROUP_GID=1000
 fi
 
 if getent group $GROUP_NAME >/dev/null 2>&1; then
   echo "a group named $GROUP_NAME already exists."
 else
-  groupadd -og $GROUP_NAME $USER_NAME
-  echo >&2 "Created group: $GROUP_NAME (gid: $USER_GID)"
+  groupadd -og $GROUP_GID $GROUP_NAME
+  echo >&2 "Created group: $GROUP_NAME (gid: $GROUP_GID)"
 fi
 
 if id -u $USER_NAME >/dev/null 2>&1; then
   echo "a user named $USER_NAME already exists."
 else
-  useradd -Mos /bin/false -u $USER_UID -g $USER_GID $USER_NAME
+  useradd -Mos /bin/false -u $USER_UID -g $GROUP_GID $USER_NAME
   echo "$USER_NAME:$USER_PASSWORD" | chpasswd
-  echo >&2 "Created user: $USER_NAME (uid: $USER_UID, gid: $USER_GID)"
+  echo >&2 "Created user: $USER_NAME (uid: $USER_UID, gid: $GROUP_GID)"
 fi
 
 if [ ! -f /etc/ssl/certs/mineos.crt ]; then
