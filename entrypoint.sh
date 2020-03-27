01f18a01155e4c303d9cb79fbd277b8fe9a3ac21
Kyle Klaus <kklaus@indemnity83.com>
2020-03-27 11:37:35 -0700

    Allow overriding of user GID
    This modification will allow the group ID of the docker user to be overridden which may resolve issues with permissions in certain docker deployments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,12 +27,22 @@ else
   USER_UID=1000
 fi
 
+if [ "$USER_GID" ]; then
+  # gid specifically provided, will overwrite 1000 default
+  if [[ "$USER_GID" =~ [^0-9] ]]; then
+    echo >&2 'USER_GID must contain only numerics [0-9]'
+    exit 1
+  fi
+else
+  USER_GID=1000
+fi
+
 if id -u $USER_NAME >/dev/null 2>&1; then
   echo "$USER_NAME already exists."
 else
-  useradd -Ms /bin/false -u $USER_UID $USER_NAME
+  useradd -Ms /bin/false -u $USER_UID -g $USER_GID $USER_NAME
   echo "$USER_NAME:$USER_PASSWORD" | chpasswd
-  echo >&2 "Created user: $USER_NAME (uid: $USER_UID)"
+  echo >&2 "Created user: $USER_NAME (uid: $USER_UID, guid: $USER_GID)"
 fi
 
 if [ ! -f /etc/ssl/certs/mineos.crt ]; then
