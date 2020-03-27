873394c39a05a0a22b2fd14ed64e55cbd34e0681
Kyle Klaus <kklaus@indemnity83.com>
2020-03-27 12:02:20 -0700

    allow duplicate UID

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ if id -u $USER_NAME >/dev/null 2>&1; then
   echo "$USER_NAME already exists."
 else
   groupadd -og $USER_GID $USER_NAME
-  useradd -Ms /bin/false -u $USER_UID -g $USER_GID $USER_NAME
+  useradd -Mos /bin/false -u $USER_UID -g $USER_GID $USER_NAME
   echo "$USER_NAME:$USER_PASSWORD" | chpasswd
   echo >&2 "Created user: $USER_NAME (uid: $USER_UID, gid: $USER_GID)"
 fi
