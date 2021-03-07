30d14730ac67f42af566af565fbbee9553ab9b1d
flareofghast <petersalzmann@gmail.com>
2021-03-08 07:06:50 +1030

    formatting

--- profiles.js
+++ profiles.js
@@ -2,14 +2,16 @@ var glob = require('glob');
 
 var profile_manifests = {};
 
-glob.sync( './profiles/*.js').forEach( function(file){
-  if(!file.match('template.js')){
-  
-  var loadedProfile = require(file);
+glob.sync('./profiles/*.js').forEach(function (file) {
+  if (!file.match('template.js')) {
 
-  var name = file.split('/')[2].split('.')[0];
-  profile_manifests[name] = loadedProfile.profile;
-}
+    var loadedProfile = require(file);
+
+    if (Object.getOwnPropertyNames(loadedProfile).length > 0) {
+      var name = file.split('/')[2].split('.')[0];
+      profile_manifests[name] = loadedProfile.profile;
+    }
+  }
 });
 
 exports.profile_manifests = profile_manifests;
