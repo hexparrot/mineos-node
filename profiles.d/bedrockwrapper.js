1831a4a938919ec9b7ed5badf2de1f656c0caf68
Jez Tucker <1470172+tucks@users.noreply.github.com>
2021-08-08 20:20:46 +0100

    firmed up bedrock wrapper config

--- profiles.d/bedrockwrapper.js
+++ profiles.d/bedrockwrapper.js
@@ -11,18 +11,16 @@ exports.profile = {
     try {
       var item = {};
 
-      item['id'] = 'mineos-bedrock-wrapper';
-      item['time'] = new Date().getTime();
-      item['releaseTime'] = new Date().getTime();
-      item['type'] = 'release';
+      item['id'] = 'bedrock-server-wrapper';
+      item['type'] = 'snapshot';
       item['group'] = 'mineosbedrockwrapper';
-      item['webui_desc'] = 'MineOS Bedrock Wrapper';
+      item['webui_desc'] = 'Bedrock Server Wrapper';
       item['weight'] = 0;
       item['filename'] = 'mineos-bedrock-wrapper-1.0-SNAPSHOT.jar';
       item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
       item['version'] = 0;
-      item['release_version'] = '';
-      item['url'] = 'https://github.com/tucks/mineos-bedrock-wrapper/blob/master/download/latest/mineos-bedrock-wrapper-1.0-SNAPSHOT.jar';
+      item['release_version'] = '1.0';
+      item['url'] = 'https://github.com/tucks/mineos-bedrock-wrapper/raw/master/download/latest/mineos-bedrock-wrapper-1.0-SNAPSHOT.jar';
 
       p.push(item);	
     } catch (e) { }
