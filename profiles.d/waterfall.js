864d080ef4d31904a42ebe21be357f6ca4d74fa0
Peter Salzmann <petersalzmann@gmail.com>
2021-11-30 15:35:59 +1030

    waterfall update to api v2

--- profiles.d/waterfall.js
+++ profiles.d/waterfall.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var fs = require('fs-extra');
 var profile = require('./template');
+var axios = require('axios');
 
 exports.profile = {
   name: 'Waterfall',
@@ -15,23 +16,35 @@ exports.profile = {
     try {
       for (var index in body.versions) {
         var version = body.versions[index];
-        var item = new profile();
 
-        item['id'] = 'waterfall-{0}-latest'.format(version);
-        item['group'] = 'waterfall';
-        item['webui_desc'] = 'Latest Waterfall build for {0}'.format(version);
-        item['weight'] = 0;
-        item['filename'] = 'waterfall.jar'.format(version);
-        item['url'] = 'https://papermc.io/api/v1/waterfall/{0}/latest/download'.format(version);
-        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
-        item['version'] = version;
-        item['release_version'] = version;
-        item['type'] = 'release'
-
-        p.push(item);
+        p.push(axios({ url: `https://papermc.io/api/v2/projects/waterfall/versions/${version}/`}).catch((err) => {
+          console.log(err);
+        }));
+        
       }
+      Promise.all(p).then(responses => {
+        p = [];
+        responses.forEach(response => {
+          var build = response.data.builds[ response.data.builds.length -1 ];
+          const splitPath = response.request.path.split('/');
+          var ver =splitPath[splitPath.length - 2];
+          var item = new profile();
+    
+          item['id'] = "Waterfall-{0}-{1}".format(ver,build);
+          item['group'] = 'waterfall';
+          item['webui_desc'] = 'Latest Waterfall build for {0}'.format(ver);
+          item['weight'] = 0;
+          item['filename'] = "waterfall-{0}-{1}.jar".format(ver,build);
+          item['url'] = `${response.request.res.responseUrl}builds/${build}/downloads/waterfall-${ver}-${build}.jar`;
+          item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
+          item['version'] = ver;
+          item['release_version'] = ver;
+          item['type'] = 'release'
+    
+          p.push(item);
+        })
+      }).then(() => { callback(null, p)});
     } catch (e) { console.log(e) }
-    callback(null, p);
   } //end handler
 }
 
