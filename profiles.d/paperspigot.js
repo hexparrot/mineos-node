95dd9c5a68ad0c6e562b4044f61b9fa981668fab
Peter Salzmann <petersalzmann@gmail.com>
2021-11-30 15:13:12 +1030

    update paper profile fetch to api v2

--- profiles.d/paperspigot.js
+++ profiles.d/paperspigot.js
@@ -1,12 +1,12 @@
-// var async = require('async');
 var path = require('path');
 var fs = require('fs-extra');
 var profile = require('./template');
+var axios = require('axios');
 
 exports.profile = {
   name: 'Paper',
   request_args: {
-    url: 'https://papermc.io/api/v1/paper',
+    url: 'https://papermc.io/api/v2/projects/paper/',
     json: true
   },
   handler: function (profile_dir, body, callback) {
@@ -15,22 +15,35 @@ exports.profile = {
     try {
       for (var index in body.versions) {
         var version = body.versions[index];
-        var item = new profile();
 
-        item['id'] = 'Paper-{0}-latest'.format(version);
-        item['group'] = 'papermc';
-        item['webui_desc'] = 'Latest Paper build for {0}'.format(version);
-        item['weight'] = 0;
-        item['filename'] = 'paperclip.jar'.format(version);
-        item['url'] = 'https://papermc.io/api/v1/paper/{0}/latest/download'.format(version);
-        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
-        item['version'] = version;
-        item['release_version'] = version;
-        item['type'] = 'release'
-
-        p.push(item);
+        p.push(axios({ url: `https://papermc.io/api/v2/projects/paper/versions/${version}/`}).catch((err) => {
+          console.log(err);
+        }));
+        
       }
+      Promise.all(p).then(responses => {
+        p = [];
+        responses.forEach(response => {
+          var build = response.data.builds[ response.data.builds.length -1 ];
+          const splitPath = response.request.path.split('/');
+          var ver =splitPath[splitPath.length - 2];
+          var item = new profile();
+    
+          item['id'] = "Paper-{0}-{1}".format(ver,build);
+          item['group'] = 'papermc';
+          item['webui_desc'] = 'Latest Paper build for {0}'.format(ver);
+          item['weight'] = 0;
+          item['filename'] = "paper-{0}-{1}.jar".format(ver,build);
+          item['url'] = `${response.request.res.responseUrl}builds/${build}/downloads/paper-${ver}-${build}.jar`;
+          item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
+          item['version'] = ver;
+          item['release_version'] = ver;
+          item['type'] = 'release'
+    
+          p.push(item);
+        })
+      }).then(() => { callback(null, p)});
+      
     } catch (e) { console.log(e) }
-    callback(null, p);
   } //end handler
 }
