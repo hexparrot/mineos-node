8f7e8c0f76b0cc024a8b9d3cee9007bbe7f0982b
Peter Salzmann <petersalzmann@gmail.com>
2021-12-18 03:04:16 +1030

    Paper waterfall profiles (#448)
    * initial commit paper and waterfall profiles
    
    * update paper profile fetch to api v2
    
    * update waterfall for api v2
    
    * waterfall update to api v2
    
    * update paper and waterfall
    
    * fixed paper and waterfall not detecting downloaded.
    
    * updated travertine

--- profiles.d/waterfall.js
+++ profiles.d/waterfall.js
@@ -1,37 +1,3 @@
-// var async = require('async');
-var path = require('path');
-var fs = require('fs-extra');
-var profile = require('./template');
+var papertemplate = require('./papertemplate');
 
-exports.profile = {
-  name: 'Waterfall',
-  request_args: {
-    url: 'https://papermc.io/api/v1/waterfall',
-    json: true
-  },
-  handler: function (profile_dir, body, callback) {
-    var p = [];
-
-    try {
-      for (var index in body.versions) {
-        var version = body.versions[index];
-        var item = new profile();
-
-        item['id'] = 'waterfall-{0}-latest'.format(version);
-        item['group'] = 'waterfall';
-        item['webui_desc'] = 'Latest Waterfall build for {0}'.format(version);
-        item['weight'] = 0;
-        item['filename'] = 'waterfall.jar'.format(version);
-        item['url'] = 'https://papermc.io/api/v1/waterfall/{0}/latest/download'.format(version);
-        item['downloaded'] = fs.existsSync(path.join(profile_dir, item.id, item.filename));
-        item['version'] = version;
-        item['release_version'] = version;
-        item['type'] = 'release'
-
-        p.push(item);
-      }
-    } catch (e) { console.log(e) }
-    callback(null, p);
-  } //end handler
-}
-
+exports.profile = papertemplate('waterfall'); 
