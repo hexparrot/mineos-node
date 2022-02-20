440328db6926f6d0a336178b7610a8b72cf5d6e9
William Dizon <wdchromium@gmail.com>
2022-02-20 01:06:54 -0700

    Update test-mineos.js

--- test/test-mineos.js
+++ test/test-mineos.js
@@ -11,8 +11,8 @@ var FS_DELAY_MS = 200;
 var PROC_START_DELAY_MS = 200;
 
 var OWNER_CREDS = {
-  uid: userid.uid(process.env.USER) || 1001,
-  gid: userid.gid(process.env.USER) || 121
+  uid: 1001,
+  gid: 121
 }
 
 function oct2dec(octal_val) {
